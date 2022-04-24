b9e1cb8334054c4568c5293ce48594f04c04b36c
MityaAndreevich <dmitry.logachev@icloud.com>
2022-04-24 19:42:21 +0300

    minor refactor

--- ToDoAppRealm/Controllers/TaskListViewController.swift
+++ ToDoAppRealm/Controllers/TaskListViewController.swift
@@ -11,26 +11,19 @@ class TaskListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-       
-        return 0
+        0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-   
-        return 0
+        0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TaskListCell", for: indexPath)
-
-      
 
         return cell
     }
--- ToDoAppRealm/Controllers/TasksViewController.swift
+++ ToDoAppRealm/Controllers/TasksViewController.swift
@@ -15,21 +15,16 @@ class TasksViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-    
-        return 0
+        0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 0
+        0
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
-
 
         return cell
     }
