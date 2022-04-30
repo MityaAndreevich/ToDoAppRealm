a138eadc01c8ceceb6d025e198c16c7a0d75eebf
MityaAndreevich <dmitry.logachev@icloud.com>
2022-04-30 10:38:02 +0300

    add mock data

--- ToDoAppRealm/Services/DataManager.swift
+++ ToDoAppRealm/Services/DataManager.swift
@@ -16,7 +16,26 @@ class DataManager {
         if !UserDefaults.standard.bool(forKey: "done") {
             UserDefaults.standard.set(true, forKey: "done")
             
+            let moviesList = TaskList(value:
+                                        ["Movies List", Date(),
+                                              [
+                                                ["Best Film Ever"],
+                                                ["Second best film ever", "Must see", Date(), true]
+                                              ]
+                                        ])
+            
             let shoppingList = TaskList()
+            shoppingList.name = "Shopping List"
+            let milk = Task()
+            milk.name = "Milk"
+            milk.note = "2L"
+            
+            let bread = Task(value: ["Bread", "", Date(), true])
+            
+            let apples = Task(value: ["name": "Apples", "note": "2kg"])
+            
+            shoppingList.tasks.append(milk)
+            shoppingList.tasks.insert(contentsOf: [bread, apples], at: 1)
         }
     }
 }
