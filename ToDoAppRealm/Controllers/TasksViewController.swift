eb1c95b27861279ab82a47ce74c5132466c95e1c
MityaAndreevich <dmitry.logachev@icloud.com>
2022-05-02 13:44:08 +0300

    add tableView methods to TasksVC

--- ToDoAppRealm/Controllers/TasksViewController.swift
+++ ToDoAppRealm/Controllers/TasksViewController.swift
@@ -11,22 +11,36 @@ class TasksViewController: UITableViewController {
     
     var taskList: TaskList!
     
+    private var currentTasks: Results<Task>!
+    private var completedTasks: Results<Task>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = taskList.name
+        currentTasks = taskList.tasks.filter("isCompleted = false")
+        completedTasks = taskList.tasks.filter("isCompleted = true")
     }
     
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        0
+        2
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        0
+        section == 0 ? currentTasks.count : completedTasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        section == 0 ? "Current Tasks" : "Completed Tasks"
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
+        let task  = indexPath.section == 0 ? currentTasks[indexPath.row] : completedTasks[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = task.name
+        content.secondaryText = task.note
+        cell.contentConfiguration = content
         
         return cell
     }
